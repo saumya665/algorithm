eb295c8d79bfd6a688ab01a7e362770dbf40003f
Saumya Singh <saumyasing@gmail.com>
2017-10-17 16:28:19 -0700

    updated with less variables

--- solutions/factorial.js
+++ solutions/factorial.js
@@ -1,10 +1,13 @@
-const factorial=(num,product=1,i=1)=>{
-  if (i==num+1){
+const factorial=(num,product=1)=>{
+  if (num==1){
       return product;
   }
   else{
-    product = product*i
+    product = product*num;
   }
-  return factorial(num,product,i+1);
+  return factorial(num-1,product);
 }
+console.log(factorial(5));
+  console.log(factorial(4));
+console.log(factorial(3));
 module.exports = factorial;

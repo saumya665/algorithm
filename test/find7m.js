defc87285df11e63a3eabfc876632bfb423ba794
Saumya Singh <saumyasing@gmail.com>
2017-10-17 11:07:19 -0700

    files for multiples

---
+++ test/find7m.js
@@ -0,0 +1,13 @@
+const solution = require('../solutions/find7m.js');
+
+const test =(num,result)=>{
+  if(solution(num)==result){
+    console.log("answer is correct");
+  }
+  else{
+    console.log("Incorrect answer");
+  }
+}
+test(12,14);
+test(20,21);
+test(20,17);

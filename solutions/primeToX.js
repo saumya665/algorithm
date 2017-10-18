75fe262e751d5fcc9b0ff8209b26ae8e1a87f122
Saumya Singh <saumyasing@gmail.com>
2017-10-18 12:07:02 -0700

    adding solution and test files

---
+++ solutions/primeToX.js
@@ -0,0 +1,20 @@
+const isPrime=(num,i=2)=>{
+  if(num===i){
+    return true;
+  }
+  if(num%i==0){
+    return false;
+  }
+  return isPrime(num,i+1)
+}
+
+const iterate=(num,arr=[])=>{
+  if(num==1){
+    return arr;
+  }
+  if (isPrime(num)==true){
+    arr.push(num);
+  }
+  return iterate(num-1,arr);
+}
+module.exports= iterate;
